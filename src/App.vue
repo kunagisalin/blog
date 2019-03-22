f4f8a8030b254884c0ac4f6ab716eff9d7e4402c
“kunagisalin” <kunagisalin@gmail.com>
2019-03-22 15:56:37 +0900

    no

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,8 @@
 </script>
 
 <style>
-
+#app{
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -1,6 +1,9 @@
 <template>
-<div class="hello">
-    <div>login</div>
+<div class="index">
+    <div id="login">login</div>
+    <div id="nav">
+      <div id="blog"><h1 class="font-weight:500;">KunagisaLin</h1></div>
+    </div>
     <router-view/>
 </div>
 </template>
@@ -9,6 +12,49 @@
 
 </script>
 
-<style scoped>
-
+<style>
+  #nav{
+    width: 100%;
+    height: 100%;
+    background-color: #d2d9d1;
+    padding: 120px 0 42px;
+    top: 0;
+    left: 0;
+    right: 0;
+    border: 0;
+    margin:0
+  }
+  #login{
+    top: 30px;
+    left: 0;
+    color: #444b44;
+    position: fixed;
+    border-radius: 5px;
+    border: 1px solid;
+    cursor: pointer;
+    display: inline-block;
+    font-weight: 400;
+    letter-spacing: .5px;
+    margin: 0;
+    text-transform: none;
+    background: 0 0;
+    text-decoration: none;
+    line-height: 34px;
+    font-size: 12px;
+    padding: 0 24px;
+  }
+  .index{
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    border: 0;
+    margin:0;
+  }
+  #blog{
+    margin:auto;
+    text-align:center;
+    color: #444b44;
+  }
 </style>
